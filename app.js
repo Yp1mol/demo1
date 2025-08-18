abef405e7f77b06bca4894bad50fd1a9a24fe648
Егор <egorhrebtov335@gmail.com>
2025-08-18 10:22:03 +0300

    Initial commit of all project files

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const serveStatic = require("serve-static");
+
+app.use(serveStatic("public"));
+app.use(bodyParser.json());
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    title: req.body.title,
+    completed: req.body.completed || false,
+  };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  todo.title = req.body.title || todo.title;
+  todo.completed = req.body.completed || todo.completed;
+  res.json(todo);
+});
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  todos.splice(index, 1);
+  res.status(204).send();
+});
+const PORT = process.env.PORT || 3200;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
